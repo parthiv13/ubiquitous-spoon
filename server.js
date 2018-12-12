94bf2cbcab6673bd3a3c931e0c7387f9e719c43d
parthiv13 <kparthiv13@gmail.com>
2018-12-12 16:14:10 +0530

    added profile links

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ var $ = cheerio.load(fs.readFileSync(path.join(__dirname, '/html.html')), {
 });
 
 writer.pipe(fs.createWriteStream(path.join(__dirname, 'leads.csv'), { flags: 'a'}));
-writer.write({ name: 'Name', designation: 'Designation', company: "Company" });
+//writer.write({ name: 'Name', designation: 'Designation', company: "Company" });
 $('li.pv5.ph2').each(function (i, elem) {
     let cs = cheerio.load(elem);
 
     name = cs("dt.result-lockup__name").text().trim();
     designation = cs("dd.result-lockup__highlight-keyword > span:nth-child(1)").text().trim();
     company = cs("span.result-lockup__position-company a span:nth-child(1)").text().trim();
+    link = "https://www.linkedin.com" + cs(".result-lockup__actions a").attr('href');
 
-    writer.write({ name: name, designation: designation, company: company })
-    //console.log(leads);
+    writer.write({ name: name, designation: designation, company: company, link: link })
+    console.log(link);
 
 });
 
